37094e98e427bc48cd881e9459dadba99eb1f40d
Yasassri Ratnayake <ycrnet@gmail.com>
2018-01-03 14:26:39 +0530

    Add resources

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dummy-scripts
 Dummy Scripts for TG execution
+
+1. Need to install AWS CLI and configure, These can be parsed as environment variables as well.
+
+AWS Access Key ID [****************QMZQ]: 
+AWS Secret Access Key [****************dV1K]: 
+Default region name [us-east-1]: us-east-1
+Default output format [None]: json
+
---
+++ deployment/infra-create.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# ----------------------------------------------------------------------------
+#
+# Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+#
+# WSO2 Inc. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# ----------------------------------------------------------------------------
+
+stackName=TGdummy
+keyPairName=tgDummy
+amiId=ami-08c64c72
+
+## This script will create the CF infrastructure
+echo "Infrastructure creation Initiated"
+
+# Create a New Keypair in AWS space and saving it to a .pem file
+output=$(aws ec2 create-key-pair --key-name $keyPairName --output json --query 'KeyMaterial' | cut -d '"' -f2)
+#aws ec2 create-key-pair --key-name tgDummy --output json | jq -r ".KeyMaterial" > key.pem
+echo -e $output > key.pem
+
+# Changing the permission of the key
+chmod 600 key.pem
+
+# Create the Stack || --parameters ParameterKey=am-id,ParameterValue=ami-08c64c72
+aws cloudformation create-stack --stack-name $stackName --template-body file://./cf-templates/add-instances.yaml --parameters ParameterKey=AMIID,ParameterValue=$amiId
+
+# TO-DO hold the script till the instance is in running state, check https://docs.aws.amazon.com/cli/latest/reference/cloudformation/wait/stack-create-complete.html
+
+
